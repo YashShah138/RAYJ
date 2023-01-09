1f71019a942094ea1224193a19b9cc21dda7986f
aidanywu <aidanywu@gmail.com>
2023-01-09 10:38:47 -0800

    frq3 api post

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator/CalculatorApiController.java
@@ -2,7 +2,7 @@ package com.nighthawk.spring_portfolio.mvc.calculator;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/calculator")
 public class CalculatorApiController {
-    @GetMapping("/")
+    @PostMapping("/")
     public ResponseEntity<String> calculate(@RequestBody final String expression) {
         try {
             Calculator calculatedExpression = new Calculator(expression);
