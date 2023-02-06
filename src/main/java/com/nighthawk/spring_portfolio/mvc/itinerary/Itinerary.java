21d2c571b965506bec04920ada3584c1e0686f77
peacekeeper6 <junlim954@gmail.com>
2023-02-06 09:01:53 -0800

    columns

--- src/main/java/com/nighthawk/spring_portfolio/mvc/itinerary/Itinerary.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/itinerary/Itinerary.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import static org.junit.Assert.fail;
+
 import javax.persistence.*;
 
 @Data  // Annotations to simplify writing code (ie constructors, setters)
@@ -12,28 +14,27 @@ import javax.persistence.*;
 @Entity // Annotation to simplify creating an entity, which is a lightweight persistence domain object. Typically, an entity represents a table in a relational database, and each entity instance corresponds to a row in that table.
 public class Itinerary {
     @Id
-    @Column(unique=true)
+    @Column(unique=true, nullable=false)
     private String itineraryName;
 
+    @Column(nullable=false)
     private String description;
-    
+
+    @Column(nullable=false)
     private String packing;
     
+    @Column(nullable=false)
     private String travel;
 
+    @Column(nullable=false)
     private String food;
 
+    @Column(nullable=false)
     private String hotel;
 
+    @Column(nullable=false)
     private String activities;
 
+    @Column(nullable=false)
     private String importantNotes;
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.AUTO)
-    // private Long id;
-
-    // @Column(unique=true)
-    // private String username;
-
-    // private String password;
 }
