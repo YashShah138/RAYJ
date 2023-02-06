4ae974d03a9f828d31045cc959798d5722b55c63
peacekeeper6 <junlim954@gmail.com>
2023-02-05 23:49:57 -0800

    hotels api

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/hotels/HotelsApiController.java
@@ -0,0 +1,64 @@
+package com.nighthawk.spring_portfolio.mvc.hotels;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController // annotation to create a RESTful web services
+@RequestMapping("/api/hotels")  //prefix of API
+public class HotelsApiController {
+    private JSONObject body; //last run result
+    private HttpStatus status; //last run status
+    String last_run = null; //last run day of month
+
+    // GET Covid 19 Stats
+    @GetMapping("/100")   //added to end of prefix as endpoint
+    public ResponseEntity<JSONObject> getHot() {
+
+        //calls API once a day, sets body and status properties
+        String today = new Date().toString().substring(0,10); 
+        if (last_run == null || !today.equals(last_run))
+        {
+            try {  //APIs can fail (ie Internet or Service down)
+                
+                //RapidAPI header
+                HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?locale=en-gb&currency=AED"))
+                    .header("X-RapidAPI-Key", "9e4650470emshc2461cc01c07b29p18b9b5jsnfa759ab87fca")
+                    .header("X-RapidAPI-Host", "booking-com.p.rapidapi.com")
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+
+                //RapidAPI request and response
+                HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+                //JSONParser extracts text body and parses to JSONObject
+                // this.body = (JSONObject) response.body();
+                this.body = (JSONObject) new JSONParser().parse(response.body());
+                this.status = HttpStatus.OK;  //200 success
+                this.last_run = "today";
+            }
+            catch (Exception e) {  //capture failure info
+                HashMap<String, String> status = new HashMap<>();
+                status.put("status", "RapidApi failure: " + e);
+
+                //Setup object for error
+                this.body = (JSONObject) status;
+                this.status = HttpStatus.INTERNAL_SERVER_ERROR; //500 error
+                this.last_run = null;
+            }
+        }
+
+        //return JSONObject in RESTful style
+        return new ResponseEntity<>(body, status);
+    }
+}
