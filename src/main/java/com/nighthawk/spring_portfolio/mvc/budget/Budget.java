5e723b4b5e184e1a15a6f8c7da65e186fae28c70
xiaoa0 <xiaoallie@gmail.com>
2023-02-07 08:51:55 -0800

    started budgeting backend

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/budget/Budget.java
@@ -0,0 +1,17 @@
+package com.nighthawk.spring_portfolio.mvc.budget;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.Math;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+
+public class Budget {
+    ArrayList<Integer> budgetingArray = new ArrayList<Integer>();
+    
+    for (int i = 0; i < budgetingArray.size(); i++) {
+        return budgetingArray.get(i);
+    }
+
+}
