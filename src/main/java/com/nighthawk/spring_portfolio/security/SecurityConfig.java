dde44a6628208932d6ed7012d29255cf73d6d4f8
aidanywu <aidanywu@gmail.com>
2023-02-16 10:05:00 -0800

    give everyone /api/person perms

--- src/main/java/com/nighthawk/spring_portfolio/security/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/security/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			// don't authenticate this particular request
 			.authorizeRequests()
 				.antMatchers("/authenticate").permitAll()
-				.antMatchers("/api/person/**").hasAnyAuthority("ROLE_USERS", "ROLE_ADMIN")
+				.antMatchers("/api/person/**").permitAll()
 				.antMatchers("/").permitAll()
 				.antMatchers("/login").permitAll()
 				.antMatchers("/admin").hasAnyAuthority("ROLE_ADMIN")
@@ -84,7 +84,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-ExposedHeaders", "*", "Authorization"))
 				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Content-Type", "Authorization", "x-csrf-token"))
 				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge", "600"))
-				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "GET", "OPTIONS", "HEAD", "DELETE"))
+				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "GET", "OPTIONS", "HEAD"))
 				// .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*", "http://localhost:8032"))
 			.and()
 				// make sure we use stateless session; session won't be used to
