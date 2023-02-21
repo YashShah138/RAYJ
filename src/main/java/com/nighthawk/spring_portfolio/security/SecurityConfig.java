cb73164bae838a4b5996b34fa63d4770bde031ef
aidanywu <aidanywu@gmail.com>
2023-02-21 01:04:34 -0800

    some permission settings

--- src/main/java/com/nighthawk/spring_portfolio/security/SecurityConfig.java
+++ src/main/java/com/nighthawk/spring_portfolio/security/SecurityConfig.java
@@ -67,19 +67,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			// We don't need CSRF for this example
 			.csrf().disable()
 			// don't authenticate this particular request
-			// .authorizeRequests()
-			// 	.antMatchers("/authenticate").permitAll()
-			// 	.antMatchers("/api/person/**").permitAll()
-			// 	.antMatchers("/").permitAll()
-			// 	.antMatchers("/login").permitAll()
-			// 	.antMatchers("/admin").hasAnyAuthority("ROLE_ADMIN")
-				// .antMatchers("/mvc/person/update/**", "/mvc/person/delete/**").authenticated()
-				// .antMatchers("/**").authenticated()
-
+			.authorizeRequests()
+				.antMatchers(HttpMethod.GET, "/").permitAll()
+				.antMatchers(HttpMethod.GET, "/login").permitAll()
+				.antMatchers(HttpMethod.POST, "/authenticate").permitAll()
+				.antMatchers(HttpMethod.POST, "/api/person/post").permitAll()
+				.antMatchers(HttpMethod.POST, "/api/person/**").hasAnyAuthority("ROLE_ADMIN")
+				.antMatchers(HttpMethod.GET, "/api/person/**").hasAnyAuthority("ROLE_ADMIN")
+				.antMatchers(HttpMethod.DELETE, "/api/person/**").hasAnyAuthority("ROLE_ADMIN")
+				.antMatchers(HttpMethod.GET, "/api/role/**").hasAnyAuthority("ROLE_ADMIN")
+				.antMatchers(HttpMethod.POST, "/api/role/**").hasAnyAuthority("ROLE_ADMIN")
+				.antMatchers(HttpMethod.DELETE, "/api/role/**").hasAnyAuthority("ROLE_ADMIN")
+				.antMatchers(HttpMethod.GET, "/admin").hasAnyAuthority("ROLE_ADMIN") // For testing purposes
 			// all other requests need to be authenticated
-				// .anyRequest().authenticated()
-			// .and()
-			.cors().and()
+				.anyRequest().authenticated()
+			.and().cors().and()
 			.headers()
 				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
 				.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-ExposedHeaders", "*", "Authorization"))
