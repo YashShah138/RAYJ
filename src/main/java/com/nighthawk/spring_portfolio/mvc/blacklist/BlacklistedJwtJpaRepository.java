75ba4e3345a078a535d582d3eacb8232c5056f5c
aidanywu <aidanywu@gmail.com>
2023-02-22 20:17:38 -0800

    remove unnecessary findAll()

--- src/main/java/com/nighthawk/spring_portfolio/mvc/blacklist/BlacklistedJwtJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/blacklist/BlacklistedJwtJpaRepository.java
@@ -6,6 +6,5 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlacklistedJwtJpaRepository extends JpaRepository<BlacklistedJwt, Integer> {
-    List<BlacklistedJwt> findAll();
     Optional<BlacklistedJwt> findByBlacklistedJwt(String string);
 }
