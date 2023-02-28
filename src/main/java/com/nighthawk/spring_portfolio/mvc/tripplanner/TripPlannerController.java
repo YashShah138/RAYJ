d4b70f7d7501eacd69a026818c6d644afea54e76
peacekeeper6 <junlim954@gmail.com>
2023-02-28 01:32:11 -0800

    fix delete

--- src/main/java/com/nighthawk/spring_portfolio/mvc/tripplanner/TripPlannerController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/tripplanner/TripPlannerController.java
@@ -29,51 +29,48 @@ public class TripPlannerController {
         return new ResponseEntity<>(repository.findAll(), HttpStatus.OK);
     }
 
-    @PostMapping("/create/{name}/{packing}/{travel}/{food}/{hotel}/{activities}/{notes}")
-    public ResponseEntity<TripPlanner> createTripPlanner(@PathVariable String name,
-            @PathVariable String packing, @PathVariable String travel, @PathVariable String food, @PathVariable String hotel, @PathVariable String activities, @PathVariable String notes) {
-        repository.saveAndFlush(new TripPlanner(null, name, packing, travel, food, hotel, activities, notes));
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
     @GetMapping("/search/{name}")
     public ResponseEntity<List<TripPlanner>> searchTripPlanner(@PathVariable String name) {
         List<TripPlanner> listings = repository.findByNameIgnoreCase(name);
         return new ResponseEntity<>(listings,HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<TripPlanner> deleteListing(@PathVariable long id) {
-        System.out.println("TripPlanner delete checkpoint 1.  id is "+id);
+    //create
+    @PostMapping("/create/{name}/{packing}/{travel}/{food}/{hotel}/{activities}/{notes}")
+    public ResponseEntity<TripPlanner> createTripPlanner(@PathVariable String name,
+            @PathVariable String packing, @PathVariable String travel, @PathVariable String food, @PathVariable String hotel, @PathVariable String activities, @PathVariable String notes) {
+        repository.saveAndFlush(new TripPlanner(null, name, packing, travel, food, hotel, activities, notes));
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    //update
+    @PutMapping("/update/{id}")
+    public ResponseEntity<TripPlanner> updateListing(@PathVariable long id, @RequestBody TripPlanner changes) {
         Optional<TripPlanner> optional = repository.findById(id);
-        System.out.println("TripPlanner delete checkpoint 2");
         if (optional.isPresent()) { // Good ID
-            System.out.println("TripPlanner delete checkpoint 3");
             TripPlanner listing = optional.get(); // value from findByID
-            System.out.println("TripPlanner delete checkpoint 4");
-            repository.deleteById(id); // value from findByID
-            System.out.println("TripPlanner delete checkpoint 5");
+            listing.setName(changes.getName()); // value from findByID
+            listing.setPacking(changes.getPacking()); // value from findByID
+            listing.setTravel(changes.getTravel()); // value from findByID
+            listing.setFood(changes.getFood()); // value from findByID
+            listing.setHotel(changes.getHotel()); // value from findByID
+            listing.setActivities(changes.getActivities()); // value from findByID
+            listing.setNotes(changes.getNotes());
+            repository.save(listing);
             return new ResponseEntity<>(listing, HttpStatus.OK); // OK HTTP response: status code, headers, and body
         }
-        System.out.println("TripPlanner delete checkpoint 6.  Bad request.");
         // Bad ID
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<TripPlanner> updateListing(@PathVariable long id, @RequestBody TripPlanner newListing) {
+    //delete
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<TripPlanner> deleteTripPlanner(@PathVariable long id) {
         Optional<TripPlanner> optional = repository.findById(id);
-        if (optional.isPresent()) { // Good ID
-            TripPlanner listing = optional.get(); // value from findByID
-            listing.setName(newListing.getName()); // value from findByID
-            listing.setPacking(newListing.getPacking()); // value from findByID
-            listing.setTravel(newListing.getTravel()); // value from findByID
-            listing.setFood(newListing.getFood()); // value from findByID
-            listing.setHotel(newListing.getHotel()); // value from findByID
-            listing.setActivities(newListing.getActivities()); // value from findByID
-            listing.setNotes(newListing.getNotes());
-            repository.save(listing);
-            return new ResponseEntity<>(listing, HttpStatus.OK); // OK HTTP response: status code, headers, and body
+        if (optional.isPresent()) {  // Good ID
+            TripPlanner activity = optional.get();  // value from findByID
+            repository.deleteById(id);  // value from findByID
+            return new ResponseEntity<>(activity, HttpStatus.OK);  // OK HTTP response: status code, headers, and body
         }
         // Bad ID
         return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
